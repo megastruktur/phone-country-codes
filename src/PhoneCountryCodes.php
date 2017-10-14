1091997f817e152d52044eb6d48131541af7b36f
Peter Varaksin <p.varaksin@intetics.com>
2017-10-15 01:56:53 +0300

    Add phone parsing functions for returning the list of phone codes.

--- src/PhoneCountryCodes.php
+++ src/PhoneCountryCodes.php
@@ -4,13 +4,59 @@ namespace megastruktur;
 
 class PhoneCountryCodes {
   
+  /**
+   * Get codes list
+   *    
+   * @return array
+   * 
+   *  "country_code" => [
+   *    "name",
+   *    "dial_code",
+   *    "code",
+   *  ]
+   */
+  public static function getCodesFullList() {
+    
+    $country_phone_codes = self::parseCodeVault();
+    
+    $codes_list = [];
+    
+    foreach ($country_phone_codes as $code) {
+      $codes_list[$code['code']] = $code;
+    }
+    
+    return $codes_list;
+    
+  }
+  
+  
+  /**
+   * Get the list of phone codes.
+   * 
+   * @return array
+   */
   public static function getCodesList() {
     
-    $json = file_get_contents(__DIR__ . '/includes/country_codes_vault.json');
-    $country_codes = json_decode($json, 1);
+    $phone_codes = self::parseCodeVault();
     
-    return $country_codes;
+    $codes_list = [];
+    foreach ($phone_codes as $c) {
+      $codes_list[] = $c['dial_code'];
+    }
+    
+    return $codes_list;
+  }
+  
+  /**
+   * Parse vault with phone codes and return array.
+   * @return array
+   */
+  private static function parseCodeVault() {
+    
+    $json = file_get_contents(__DIR__ . '/includes/country_codes_vault.json');
+    return json_decode($json, 1);
     
   }
   
 }
+
